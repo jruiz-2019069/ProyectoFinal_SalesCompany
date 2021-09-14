f97b01064183994519e46aaa6469ad20050a000c
dquan-2017024 <dquan-2017024@kinal.edu.gt>
2021-09-13 18:06:10 -0600

    Se agregó Modelo y ModeloDAO de Producto. (dquan-2017024)

---
+++ ProyectoFinal_SalesCompany/src/java/modelo/Producto.java
@@ -0,0 +1,62 @@
+package modelo;
+
+public class Producto {
+    private int codigoProducto;
+    private String nombreProducto;
+    private String precio;
+    private String stock;
+    private String estado;
+
+    public Producto() {
+    }
+
+    public Producto(int codigoProducto, String nombreProducto, String precio, String stock, String estado) {
+        this.codigoProducto = codigoProducto;
+        this.nombreProducto = nombreProducto;
+        this.precio = precio;
+        this.stock = stock;
+        this.estado = estado;
+    }
+
+    public int getCodigoProducto() {
+        return codigoProducto;
+    }
+
+    public void setCodigoProducto(int codigoProducto) {
+        this.codigoProducto = codigoProducto;
+    }
+
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public void setNombreProducto(String nombreProducto) {
+        this.nombreProducto = nombreProducto;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    public String getStock() {
+        return stock;
+    }
+
+    public void setStock(String stock) {
+        this.stock = stock;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+    
+}
---
+++ ProyectoFinal_SalesCompany/src/java/modelo/ProductoDAO.java
@@ -0,0 +1,114 @@
+package modelo;
+
+import confi.Conexion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductoDAO {
+
+    Conexion cn = new Conexion();
+    Connection con;
+    PreparedStatement ps;
+    ResultSet rs;
+    int resp;
+
+    //Método Listar
+    public List listar() {
+        String sql = "Select * from Producto";
+        List<Producto> listaProducto = new ArrayList<>();
+        try {
+            con = cn.Conexion();
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                Producto pr = new Producto();
+                pr.setCodigoProducto(rs.getInt(1));
+                pr.setNombreProducto(rs.getString(2));
+                pr.setPrecio(rs.getString(3));
+                pr.setStock(rs.getString(4));
+                pr.setEstado(rs.getString(5));
+                listaProducto.add(pr);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return listaProducto;
+    }
+
+    //Método Agregar
+    public int agregar(Producto pr) {
+        String sql = "INSERT INTO Producto(nombreProducto, precio, stock, estado) VALUES (?,?,?,?)";
+        try {
+            con = cn.Conexion();
+            ps = con.prepareStatement(sql);
+            ps.setString(1, pr.getNombreProducto());
+            ps.setString(2, pr.getPrecio());
+            ps.setString(3, pr.getStock());
+            ps.setString(4, pr.getEstado());
+            ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("No se pudo agregar el producto");
+        }
+
+        return resp;
+    }
+
+    //Buscar por Código
+    public Producto listarcodigoProducto(int id) {
+        Producto pr = new Producto();
+        String sql = "Select * from Producto where codigoProducto = " + id;
+        try {
+            con = cn.Conexion();
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                pr.setCodigoProducto(rs.getInt(1));
+                pr.setNombreProducto(rs.getString(2));
+                pr.setPrecio(rs.getString(3));
+                pr.setStock(rs.getString(4));
+                pr.setEstado(rs.getString(5));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return pr;
+    }
+
+    //Método de Editar 
+    public int actualizar(Producto pr) {
+        String sql = "update Producto set nombreProducto = ?, precio = ?, stock = ?, estado = ? where codigoProducto = ?";
+
+        try {
+            con = cn.Conexion();
+            ps = con.prepareStatement(sql);
+            ps.setString(1, pr.getNombreProducto());
+            ps.setString(2, pr.getPrecio());
+            ps.setString(3, pr.getStock());
+            ps.setString(4, pr.getEstado());
+            ps.setInt(5, pr.getCodigoProducto());
+            ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return resp;
+    }
+
+    //Método para Eliminar
+    public void eliminar(int id) {
+        String sql = "delete from Producto where codigoProducto =" + id;
+        try {
+            con = cn.Conexion();
+            ps = con.prepareStatement(sql);
+            ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
